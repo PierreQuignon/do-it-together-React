903a78254a668ac844e4d491ada8b72a681edc28
Pierre Quignon <p.quignon.pro@gmail.com>
2023-02-21 16:49:30 +0100

    built the dataset file

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
 // import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useParams } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import About from "./pages/About";
 import Error from "./pages/Error";
 import Index from "./pages/Index";
 import Workshop from "./pages/Workshop";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import "./index.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<Error />}/>
-        <Route path="/index" element={<Index />}/>
-        <Route path="/workshop/:workshopParams" element={<Workshop />}/>
-      </Routes>
+        <div className="border border-black">
+          <Header />
+        </div>
+        <div>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<Error />} />
+            <Route path="/index" element={<Index />} />
+            <Route path="/workshop/:workshopParams" element={<Workshop />} />
+          </Routes>
+        </div>
+        <div className="border border-black">
+          <Footer />
+        </div>
     </BrowserRouter>
   );
 }
--- src/pages/Workshop.tsx
+++ src/pages/Workshop.tsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 
 function Workshop() {
   const { workshopParams } = useParams();
-  const workshopId = parseInt(workshopParams);
+  const workshopId = parseInt(workshopParams!);// le point d'explamation à pour focntion de 
 
   return <div>Workshop { workshopId }</div>;
 }
