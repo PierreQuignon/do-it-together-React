81eb1305434958697191ea1a01fe5242f61fcd8a
Pierre Quignon <p.quignon.pro@gmail.com>
2023-02-24 13:05:13 +0100

    wrote some content for the About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,20 @@
 function About() {
   return(
-    <div>About</div>
+    <>
+    <h1>"Partager un moment de bricolage convivial tout en gagnant de l'argent"</h1>
+    <br />
+    <p>Tel est l'objectif de <strong>Do It Together.</strong>Tous les passionné(e)s de bricolage habitant en ville et dans un logement dépourvu de garage comprendrons la frustration de ne pas pouvoir bricoler lorsqu'ils ou elles en ressentent le besoin.</p>
+    <p>Grâce à <strong>Do It Together</strong>, louez un atelier à moindre prix près de chez vous et bénéficiez de l'outillage dont vous avez besoin tout en profitant d'un moment d'échange et de partage avec le loueur.</p>
+    <br />
+    <h2>"Gagner de l'argent en louant son atelier"</h2>
+    <p>Avec le cout de la vie qui ne cesse d'augmenter, pourquoi ne pas louer votre atelier ? Vous vous en servez peu n'est ce pas ? Il est alors facile de mettre en ligne votre bien sur notre application et cela vous permettrait d'arrondir vos fins de mois sans le moindre effrot.</p>
+    <br />
+    <h2>"Rencontrer des personnes aussi passionées de bricolage que vous"</h2>
+    <p>Le bricolage est votre passion et vous aimez rencontrer de nouvelles personne ? Il est donc temps de mettre votre atelier en location pour partager des moments conviviaux avec des bricoleurs de votre secteur. Si vous aimez transmettre vos connaissance, vous pourrez alors en faire profiter à des bricoleurs en quête de savoir.</p>
+    <br />
+    <h2>"Bricoler à moindre coût prêt de chez vous"</h2>
+    <p>C'est la motiviation première qui est à l'origine de cette application. Il est très compliqué à l'heure actuelle de posséder un logement équipé d'un garage prêt d'une grande villes par faute de budjet. Avec l'application <strong>Do It Together</strong></p>
+    </>
   )
 }
 
