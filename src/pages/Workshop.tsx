1efbe32eb5fea7dd2d0608ac7276eeab2aba9bf0
Pierre Quignon <p.quignon.pro@gmail.com>
2023-06-05 19:07:07 +0200

    kept to add style to the workshop page

--- src/pages/Workshop.tsx
+++ src/pages/Workshop.tsx
@@ -5,9 +5,11 @@ import { Workshop as workshopType } from "./Workshops";
 import ModalBooking from "../components/ModalBooking";
 import "../style/Modal.css";
 import "../style/App.css";
-import SlideShow from "../components/SlideShow";
 import "../style/Slide.css";
 import "../style/Workshop.css";
+import menuiserie1 from "../assets/menuiserie/menuiserie1.jpg";
+import menuiserie2 from "../assets/menuiserie/menuiserie2.jpg";
+import menuiserie3 from "../assets/menuiserie/menuiserie3.jpeg";
 
 const Workshop: FC = () => {
   const [workshopTargeted, setWorkshopTargeted] = useState<workshopType[] | []>(
@@ -59,42 +61,69 @@ const Workshop: FC = () => {
               <div className="rating-container">
                 <i className="fa-regular fa-star"></i>
                 <li>{workshop.rating}</li>
-                <li className="workshop-comment">( {workshop.comment} commentaires )</li>
+                <li className="workshop-comment">
+                  ( {workshop.comment} commentaires )
+                </li>
+                <li>
+                  <u>{workshop.location}</u>
+                </li>
               </div>
-              <li>Adresse: {workshop.location}</li>
-              <li>{workshop.content}</li>
-              <li>Surface: {workshop.squareMeter}m2</li>
-              <li>
-                Electricité{" "}
-                {workshop.electricity ? (
-                  <i className="fa-regular fa-square-check"></i>
-                ) : (
-                  <i className="fa-sharp fa-regular fa-square"></i>
-                )}
-              </li>
-              <li>
-                Point d'eau{" "}
-                {workshop.water ? (
-                  <i className="fa-regular fa-square-check"></i>
-                ) : (
-                  <i className="fa-sharp fa-regular fa-square"></i>
-                )}
-              </li>
-              <li>Prix: {workshop.price}€/jour</li>
-              <li>Loueur: {workshop.owner}</li>
-              <li>Contact: {workshop.phone}</li>
+              <div className="images-worshop-container">
+                <img
+                  src={menuiserie1}
+                  alt="image1"
+                  className="workshop-image1"
+                />
+                <div>
+                  <img
+                    src={menuiserie2}
+                    alt="image2"
+                    className="workshop-image2"
+                  />
+                  <img
+                    src={menuiserie3}
+                    alt="image3"
+                    className="workshop-image3"
+                  />
+                </div>
+              </div>
+              <div className="details-workshop-text">
+                <li className="workshop-content">{workshop.content}</li>
+                <li>{workshop.squareMeter}m2</li>
+                <li>
+                  Electricité{" "}
+                  {workshop.electricity ? (
+                    <i className="fa-regular fa-square-check"></i>
+                  ) : (
+                    <i className="fa-sharp fa-regular fa-square"></i>
+                  )}
+                </li>
+                <li>
+                  Point d'eau{" "}
+                  {workshop.water ? (
+                    <i className="fa-regular fa-square-check"></i>
+                  ) : (
+                    <i className="fa-sharp fa-regular fa-square"></i>
+                  )}
+                </li>
+                <li>
+                  <b>{workshop.price}</b>€ par jour
+                </li>
+                <li>Loueur: {workshop.owner}</li>
+                <li>Contact: {workshop.phone}</li>
+              </div>
             </>
           ))}
         </ul>
+        <button
+          className="btn-style-1"
+          onClick={() => {
+            openModal();
+          }}
+        >
+          Réserver
+        </button>
       </div>
-      <button
-        className="btn-style-1"
-        onClick={() => {
-          openModal();
-        }}
-      >
-        Réserver
-      </button>
       <ModalBooking
         workshopTargeted={workshopTargeted}
         closeModal={closeModal}
