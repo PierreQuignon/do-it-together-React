44674e1c1b40559685ed7615b5e683985ec52ba9
Pierre Quignon <p.quignon.pro@gmail.com>
2023-06-07 15:02:09 +0200

    made the panel with the workshop options

--- src/components/OptionsWorkshop.tsx
+++ src/components/OptionsWorkshop.tsx
@@ -1,20 +1,16 @@
 import { FC } from "react";
 import { Workshop } from "../pages/Workshops";
 import { IoMdFlash } from "react-icons/io";
-import { IoMdFlashOff } from "react-icons/io";
 import { BsLightbulb } from "react-icons/bs";
-import { BsLightbulbOff } from "react-icons/bs";
 import { FaTools } from "react-icons/fa";
 import { LuParkingCircle } from "react-icons/lu";
-import { LuParkingCircleOff } from "react-icons/lu";
 import { FaDog } from "react-icons/fa";
 import { BiWifi } from "react-icons/bi";
-import { BiWifiOff } from "react-icons/bi";
 import { TbWind } from "react-icons/tb";
-import { TbWindOff } from "react-icons/tb";
 import { GiSoundOn } from "react-icons/gi";
-import { GiSoundOff } from "react-icons/gi";
-
+import "../style/OptionsWorkshop.css";
+import { BiArea } from "react-icons/bi";
+import { IoIosWater } from "react-icons/io";
 
 interface optionsWorkshopProps {
   workshopTargeted: Workshop[];
@@ -23,86 +19,103 @@ interface optionsWorkshopProps {
 const OptionsWorkshop: FC<optionsWorkshopProps> = ({ workshopTargeted }) => {
   return (
     <>
-      <p>Caratéristiques :</p>
+      <p className="options-title">Ce que propose cet atelier</p>
       <div className="options-container">
         {workshopTargeted.map((workshop: Workshop) => (
           <>
-            <div>{workshop.squareMeter}</div>
+            <div className="icon-text-container">
+              <BiArea className="icon-option" />
+              {workshop.squareMeter} m2
+            </div>
             <div>
               {workshop.electricity ? (
-                <IoMdFlash className="elec-icon" />
-              ) : (
-                <IoMdFlashOff className="no-elec-icon" />
-              )}
+                <>
+                  <div className="icon-text-container">
+                    <IoMdFlash className="icon-option" />
+                    <p>Prises électriques</p>
+                  </div>
+                </>
+              ) : null}
+            </div>
+            <div>
+              {workshop.water ? (
+                <>
+                  <div className="icon-text-container">
+                    <IoIosWater className="icon-option" />
+                    <p>Point d'eau</p>
+                  </div>
+                </>
+              ) : null}
             </div>
             <div>
               {workshop.light ? (
-                <BsLightbulb className="light-icon" />
-              ) : (
-                <BsLightbulbOff className="no-light-icon" />
-              )}
+                <>
+                  <div className="icon-text-container">
+                    <BsLightbulb className="icon-option" />
+                    <p>Eclairage adapté au bricolage</p>
+                  </div>
+                </>
+              ) : null}
             </div>
             <div>
               {workshop.maintools ? (
                 <>
-                  <FaTools className="maintools-icon" />
-                  <p>
-                    Principaux outils <b>fournies</b>
-                  </p>
+                  <div className="icon-text-container">
+                    <FaTools className="icon-option" />
+                    <p>Principaux outils mis à disposition</p>
+                  </div>
                 </>
-              ) : (
-                <>
-                  <FaTools className="maintools-icon" />
-                  <p>
-                    Principaux outils <b>non fournies</b>
-                  </p>
-                </>
-              )}
+              ) : null}
             </div>
             <div>
               {workshop.parking ? (
-                <LuParkingCircle className="parking-icon" />
-              ) : (
-                <LuParkingCircleOff className="no-parking-icon" />
-              )}
+                <>
+                  <div className="icon-text-container">
+                    <LuParkingCircle className="icon-option" />
+                    <p>Parking à proximité</p>
+                  </div>
+                </>
+              ) : null}
             </div>
             <div>
               {workshop.animals ? (
                 <>
-                  <FaDog className="dog-icon" />
-                  <p>
-                    Animaux de compagnies <b>acceptés</b>
-                  </p>
+                  <div className="icon-text-container">
+                    <FaDog className="icon-option" />
+                    <p>Animaux de compagnies acceptés</p>
+                  </div>
                 </>
-              ) : (
-                <>
-                  <FaDog className="dog-icon" />
-                  <p>
-                  Animaux de compagnies <b>interdits</b>
-                  </p>
-                </>
-              )}
+              ) : null}
             </div>
             <div>
               {workshop.parking ? (
-                <BiWifi className="wifi-icon" />
-              ) : (
-                <BiWifiOff className="no-wifi-icon" />
-              )}
+                <>
+                  <div className="icon-text-container">
+                    <BiWifi className="icon-option" />
+                    <p>Wifi à disposition</p>
+                  </div>
+                </>
+              ) : null}
             </div>
             <div>
               {workshop.pressurizedair ? (
-                <TbWind className="air-icon" />
-              ) : (
-                <TbWindOff className="no-air-icon" />
-              )}
+                <>
+                  <div className="icon-text-container">
+                    <TbWind className="icon-option" />
+                    <p>Air comprimé à disposition</p>
+                  </div>
+                </>
+              ) : null}
             </div>
             <div>
               {workshop.soundproof ? (
-                <GiSoundOn className="soundproof-icon" />
-              ) : (
-                <GiSoundOff className="no-soundproof-icon" />
-              )}
+                <>
+                  <div className="icon-text-container">
+                    <GiSoundOn className="icon-option" />
+                    <p>Pièce insonorisé</p>
+                  </div>
+                </>
+              ) : null}
             </div>
           </>
         ))}
